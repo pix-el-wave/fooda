2a59f1fb1b5b8c422aac3ccabda31cbdeb0578d5
hyeonnkang <hyeonkang.def@gmail.com>
2023-09-25 15:51:25 +0900

    init : 프로젝트 생성

---
+++ src/main/java/inha/capstone/fooda/FoodaApplication.java
@@ -0,0 +1,13 @@
+package inha.capstone.fooda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/inha/capstone/fooda/FoodaApplicationTests.java
@@ -0,0 +1,13 @@
+package inha.capstone.fooda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
