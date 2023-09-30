f00ecd24abd5d23f4d95536ba2e385c67cfd335d
hyeonnkang <hyeonkang.def@gmail.com>
2023-09-30 21:44:59 +0900

    feat: [#7] 기본 Response 설정

---
+++ src/main/java/inha/capstone/fooda/domain/common/response/BaseResponse.java
@@ -0,0 +1,13 @@
+package inha.capstone.fooda.domain.common.response;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public class BaseResponse {
+
+    @Schema(example = "true", description = "요청 처리 성공 시 true, 실패 시 false")
+    private boolean isSuccess;
+}
