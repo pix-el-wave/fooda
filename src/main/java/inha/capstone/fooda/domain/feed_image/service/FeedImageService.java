d5561e8f59449033b01e95d945bfb8ac1c59831a
pseong <dnrseong@gmail.com>
2023-10-15 00:31:28 +0900

    feat: [#22] ImageIO 사용하여 업로드 한 파일이 이미지인지 검사하는 로직 추가

--- src/main/java/inha/capstone/fooda/domain/feed_image/service/FeedImageService.java
+++ src/main/java/inha/capstone/fooda/domain/feed_image/service/FeedImageService.java
@@ -5,6 +5,7 @@ import inha.capstone.fooda.domain.feed.entity.Feed;
 import inha.capstone.fooda.domain.feed.entity.Menu;
 import inha.capstone.fooda.domain.feed.repository.FeedRepository;
 import inha.capstone.fooda.domain.feed_image.entity.FeedImage;
+import inha.capstone.fooda.domain.feed_image.exception.NotImageFileExcpetion;
 import inha.capstone.fooda.domain.feed_image.repository.FeedImageRepository;
 import inha.capstone.fooda.domain.member.entity.Member;
 import lombok.RequiredArgsConstructor;
@@ -12,22 +13,30 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.imageio.ImageIO;
+import java.awt.*;
 import java.io.IOException;
 import java.util.List;
 import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
-@Transactional(readOnly = true)
+@Transactional
 public class FeedImageService {
     private final FeedImageRepository feedImageRepository;
+    private final FeedRepository feedRepository;
     private final S3Service s3Service;
 
-    public void uploadFeedImage(MultipartFile img, Feed feed) throws IOException {
+    public void uploadFeedImage(MultipartFile img, Long feedId) throws IOException {
+
+        // 이미지 파일인지 검사하는 로직
+        Image image = ImageIO.read(img.getInputStream());
+        if (image == null) throw new NotImageFileExcpetion();
+
         String fileName = UUID.randomUUID().toString();
         String url = s3Service.upload(img, fileName);
         FeedImage feedImage = FeedImage.builder()
-                .feed(feed)
+                .feed(feedRepository.getReferenceById(feedId))
                 .fileNameStored(fileName)
                 .fileName(img.getOriginalFilename())
                 .url(url)
