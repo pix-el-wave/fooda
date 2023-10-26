108e13bce275bc197d53e2ded95763de4c84ab24
pseong <dnrseong@gmail.com>
2023-10-26 20:43:07 +0900

    docs: [#30] feed 업로드 api 관련 swagger 설정

--- src/main/java/inha/capstone/fooda/domain/feed/dto/PostFeedResDto.java
+++ src/main/java/inha/capstone/fooda/domain/feed/dto/PostFeedResDto.java
@@ -15,6 +15,7 @@ public class PostFeedResDto {
     @Schema(example = "3", description = "업로드한 피드의 ID")
     private Long feedId;
 
+    @Schema(example = "[{\"no\":1,\"name\":\"Pizza\",\"size\":200,\"energy\":250.5,\"protein\":10.2,\"province\":5.5,\"carbohydrate\":30.0,\"calcium\":50.2,\"salt\":1.5}, {\"no\":2,\"name\":\"Burger\",\"size\":150,\"energy\":300.0,\"protein\":12.5,\"province\":6.0,\"carbohydrate\":25.0,\"calcium\":40.5,\"salt\":1.8}]", description = "이미지에서 추출된 음식 목록")
     List<FoodListResDto> foodList;
 
     public PostFeedResDto(UploadFeedDto uploadFeedDto) {
