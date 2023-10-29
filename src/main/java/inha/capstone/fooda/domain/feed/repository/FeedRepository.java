d44625e61b5ca045da0bd489cacb10e20c0c8c41
pseong <dnrseong@gmail.com>
2023-10-30 01:52:50 +0900

    feat: [#36] N+1 문제 해결위해 fetch join으로 foods 가져오는 쿼리 추가

--- src/main/java/inha/capstone/fooda/domain/feed/repository/FeedRepository.java
+++ src/main/java/inha/capstone/fooda/domain/feed/repository/FeedRepository.java
@@ -4,9 +4,14 @@ import inha.capstone.fooda.domain.feed.entity.Feed;
 import inha.capstone.fooda.domain.friend.entity.Friend;
 import inha.capstone.fooda.domain.member.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.List;
 
 public interface FeedRepository extends JpaRepository<Feed, Long> {
 
+    @Query("SELECT DISTINCT t FROM Feed t LEFT OUTER JOIN FETCH t.foods WHERE t.createdAt BETWEEN :startDate AND :endDate AND t.member.id = :memberId")
+    public List<Feed> findAllByCreatedAtBetweenAndMemberIdUsingFetchJoin(LocalDateTime startDate, LocalDateTime endDate, Long memberId);
 }
