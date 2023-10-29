011d5d5d8700f9ea48ae705c1afe17cd8ecfd209
pseong <dnrseong@gmail.com>
2023-10-30 04:05:08 +0900

    feat: [#37] 팔로잉 목록을 기반으로 피드 가져오는 쿼리 생성

--- src/main/java/inha/capstone/fooda/domain/feed/repository/FeedRepository.java
+++ src/main/java/inha/capstone/fooda/domain/feed/repository/FeedRepository.java
@@ -1,12 +1,9 @@
 package inha.capstone.fooda.domain.feed.repository;
 
 import inha.capstone.fooda.domain.feed.entity.Feed;
-import inha.capstone.fooda.domain.friend.entity.Friend;
-import inha.capstone.fooda.domain.member.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.List;
 
@@ -14,4 +11,7 @@ public interface FeedRepository extends JpaRepository<Feed, Long> {
 
     @Query("SELECT DISTINCT t FROM Feed t LEFT OUTER JOIN FETCH t.foods LEFT OUTER JOIN FETCH t.member WHERE t.createdAt BETWEEN :startDate AND :endDate AND t.member.id = :memberId")
     public List<Feed> findAllByCreatedAtBetweenAndMemberIdUsingFetchJoin(LocalDateTime startDate, LocalDateTime endDate, Long memberId);
+
+    @Query("SELECT DISTINCT t FROM Feed t LEFT OUTER JOIN FETCH t.foods LEFT OUTER JOIN FETCH t.member WHERE t.member.id = :memberId OR t.member.id IN (SELECT f.follower.id FROM Friend f WHERE f.following.id = :memberId)")
+    public List<Feed> findFeedsByFollowing(Long memberId);
 }
