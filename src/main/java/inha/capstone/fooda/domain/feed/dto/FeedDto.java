af8914570a7a6bbca689c21f93316803d255ca65
pseong <dnrseong@gmail.com>
2023-10-30 04:03:44 +0900

    feat: [#37] FeedDto에 피드 작성자 별명과 id 추가

--- src/main/java/inha/capstone/fooda/domain/feed/dto/FeedDto.java
+++ src/main/java/inha/capstone/fooda/domain/feed/dto/FeedDto.java
@@ -18,12 +18,20 @@ public class FeedDto {
     @Schema(description = "생성 날짜")
     private LocalDate createdAt;
 
+    @Schema(description = "피드 작성자 별명")
+    private String name;
+
+    @Schema(description = "피드 작성자 ID")
+    private String username;
+
     private List<FoodListResDto> foods;
 
     public static FeedDto from(Feed feed) {
         FeedDto feedDto = new FeedDto();
         feedDto.setId(feed.getId());
         feedDto.setCreatedAt(feed.getCreatedAt().toLocalDate());
+        feedDto.setName(feed.getMember().getName());
+        feedDto.setUsername(feed.getMember().getUsername());
         List<FoodListResDto> foodDTOs = feed.getFoods().stream()
                 .map(FoodListResDto::from)
                 .toList();
