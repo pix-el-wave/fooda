69fab402ad60a27ca67c532b146c6b57671c6c8c
pseong <dnrseong@gmail.com>
2023-11-01 00:24:35 +0900

    feat: [#36] fetch join 두 번 사용하기 위해 EntityGrpah 사용

--- src/main/java/inha/capstone/fooda/domain/feed/repository/FeedRepository.java
+++ src/main/java/inha/capstone/fooda/domain/feed/repository/FeedRepository.java
@@ -3,6 +3,7 @@ package inha.capstone.fooda.domain.feed.repository;
 import inha.capstone.fooda.domain.feed.entity.Feed;
 import inha.capstone.fooda.domain.friend.entity.Friend;
 import inha.capstone.fooda.domain.member.entity.Member;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
@@ -12,6 +13,7 @@ import java.util.List;
 
 public interface FeedRepository extends JpaRepository<Feed, Long> {
 
-    @Query("SELECT DISTINCT t FROM Feed t LEFT OUTER JOIN FETCH t.foods WHERE t.createdAt BETWEEN :startDate AND :endDate AND t.member.id = :memberId")
+    @EntityGraph(attributePaths = {"foods", "feedImages"})
+    @Query("SELECT DISTINCT t FROM Feed t WHERE t.createdAt BETWEEN :startDate AND :endDate AND t.member.id = :memberId")
     public List<Feed> findAllByCreatedAtBetweenAndMemberIdUsingFetchJoin(LocalDateTime startDate, LocalDateTime endDate, Long memberId);
 }
