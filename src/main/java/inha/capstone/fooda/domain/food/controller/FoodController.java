87b4f4788561ba07099fcf6bb6686e665f4e1eb0
pseong <dnrseong@gmail.com>
2023-11-27 02:25:26 +0900

    feat: [#58] 영양소 분석 API 개발

--- src/main/java/inha/capstone/fooda/domain/food/controller/FoodController.java
+++ src/main/java/inha/capstone/fooda/domain/food/controller/FoodController.java
@@ -1,10 +1,13 @@
 package inha.capstone.fooda.domain.food.controller;
 
 import inha.capstone.fooda.domain.common.response.DataResponse;
+import inha.capstone.fooda.domain.food.dto.PostAnalyzeReqDto;
+import inha.capstone.fooda.domain.food.dto.PostAnalyzeResDto;
 import inha.capstone.fooda.domain.food.dto.PostFoodReqDto;
 import inha.capstone.fooda.domain.food.dto.PostFoodResDto;
 import inha.capstone.fooda.domain.food.service.FoodService;
 import inha.capstone.fooda.security.FoodaPrinciple;
+import inha.capstone.fooda.utils.FoodAnalyzeResDto;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
@@ -13,10 +16,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
 
@@ -40,6 +40,23 @@ public class FoodController {
 
         return new ResponseEntity<>(
                 new DataResponse<>(new PostFoodResDto(true)),
+                HttpStatus.OK
+        );
+    }
+
+    @Operation(
+            summary = "일일 영양소 분석 API",
+            description = "<p>영양소 섭취량과 기준치를 분석하여 제공합니다.</p>"
+    )
+    @PostMapping("/analyze")
+    public ResponseEntity<DataResponse<PostAnalyzeResDto>> analyze(
+            @Parameter(hidden = true) @AuthenticationPrincipal FoodaPrinciple principle,
+            @Valid @RequestBody PostAnalyzeReqDto postAnalyzeReqDto
+    ) throws IOException {
+        String analyze = foodService.analyzeFood(principle.getMemberId(), postAnalyzeReqDto.getDate());
+
+        return new ResponseEntity<>(
+                new DataResponse<>(new PostAnalyzeResDto(analyze)),
                 HttpStatus.OK
         );
     }
--- src/main/java/inha/capstone/fooda/domain/food/service/FoodService.java
+++ src/main/java/inha/capstone/fooda/domain/food/service/FoodService.java
@@ -2,14 +2,18 @@ package inha.capstone.fooda.domain.food.service;
 
 import inha.capstone.fooda.domain.feed.entity.Feed;
 import inha.capstone.fooda.domain.feed.repository.FeedRepository;
+import inha.capstone.fooda.domain.food.dto.NutrientDto;
 import inha.capstone.fooda.domain.food.entity.Food;
 import inha.capstone.fooda.domain.food.repository.FoodRepository;
-import inha.capstone.fooda.utils.FoodListResDto;
+import inha.capstone.fooda.utils.*;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.io.IOException;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Service
@@ -18,6 +22,7 @@ import java.util.List;
 public class FoodService {
     private final FoodRepository foodRepository;
     private final FeedRepository feedRepository;
+    private final AICommunicationUtils aiCommunicationUtils;
 
     @Transactional
     public void uploadFood(Long feedId, List<FoodListResDto> foodList) throws IOException {
@@ -28,5 +33,16 @@ public class FoodService {
                 .toList();
 
         foodRepository.saveAll(foods);
+    }
+
+    public String analyzeFood(Long memberId, LocalDate date) throws IOException {
+        String s = date.atStartOfDay().toString();
+        String s2 = date.plusDays(1).atStartOfDay().toString();
+        NutrientDto now = foodRepository.findSumOfCalDay(memberId, date.atStartOfDay(), date.plusDays(1).atStartOfDay());
+        NutrientDto to = NutrientDto.baseNutrient();
+
+        FoodAnalyzeReqDto foodAnalyzeReqDto = FoodAnalyzeReqDto.from(now, to);
+
+        return aiCommunicationUtils.requestFoodAnalyze(foodAnalyzeReqDto).getData().getAnalyze();
     }
 }
