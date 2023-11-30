62a7d66b8752fa832c9d2e079191efde97d6b502
pseong <dnrseong@gmail.com>
2023-12-01 01:51:46 +0900

    feat: [#62] 사용자의 키, 몸무게, 나이, 성별에 따른 하루 영양 섭취량 계산

--- src/main/java/inha/capstone/fooda/domain/food/service/FoodService.java
+++ src/main/java/inha/capstone/fooda/domain/food/service/FoodService.java
@@ -7,11 +7,15 @@ import inha.capstone.fooda.domain.food.entity.Food;
 import inha.capstone.fooda.domain.food.exception.NotFoundFeedException;
 import inha.capstone.fooda.domain.food.repository.FoodRepository;
 import inha.capstone.fooda.domain.food.exception.MemberNotEqualFeedMemberException;
+import inha.capstone.fooda.domain.member.entity.Gender;
+import inha.capstone.fooda.domain.member.entity.Member;
+import inha.capstone.fooda.domain.member.repository.MemberRepository;
 import inha.capstone.fooda.utils.FoodListResDto;
 import inha.capstone.fooda.utils.*;
 import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -22,6 +26,7 @@ import java.math.RoundingMode;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -29,6 +34,7 @@ import java.util.List;
 public class FoodService {
     private final FoodRepository foodRepository;
     private final FeedRepository feedRepository;
+    private final MemberRepository memberRepository;
     private final AICommunicationUtils aiCommunicationUtils;
 
     @Transactional
@@ -51,7 +57,36 @@ public class FoodService {
 
     public String analyzeFood(Long memberId, LocalDate date) throws IOException {
         NutrientDto now = foodRepository.findSumOfCalDay(memberId, date.atStartOfDay(), date.plusDays(1).atStartOfDay());
-        NutrientDto to = NutrientDto.baseNutrient();
+
+        Member member = memberRepository.findById(memberId)
+                .orElseThrow(() -> new UsernameNotFoundException("유저가 존재하지 않습니다."));
+
+        // 일일 에너지 요구량 계산
+        double bmr = 0;
+        if (member.getGender().equals(Gender.MALE)) {
+            bmr = 10 * member.getWeight() + 6.25 * member.getHeight() - 5 * member.getAge() + 5;
+        } else {
+            bmr = 10 * member.getWeight() + 6.25 * member.getHeight() - 5 * member.getAge() - 161;
+        }
+
+        // 예를 들어, 활동 수준을 중간으로 가정 (1.55)
+        double dailyCalories = bmr * 1.55;
+
+        // 영양소 권장량 계산
+        double protein = member.getWeight() * 0.8; // 단백질: 체중 1kg 당 0.8g
+        double fat = dailyCalories * 0.25 / 9; // 지방: 총 칼로리의 25%
+        double carbs = dailyCalories * 0.5 / 4; // 탄수화물: 총 칼로리의 50%
+        double calcium = 1000; // 칼슘: 일반적으로 1000mg
+        double salt = 6; // 소금: 하루 6g 미만
+
+        NutrientDto to = NutrientDto.builder()
+            .calcium(BigDecimal.valueOf(calcium))
+            .carbs(BigDecimal.valueOf(carbs))
+            .energy(BigDecimal.valueOf(dailyCalories))
+            .fat(BigDecimal.valueOf(fat))
+            .protein(BigDecimal.valueOf(protein))
+            .salt(BigDecimal.valueOf(salt))
+            .build();
 
         FoodAnalyzeReqDto foodAnalyzeReqDto = FoodAnalyzeReqDto.from(now, to);
 
